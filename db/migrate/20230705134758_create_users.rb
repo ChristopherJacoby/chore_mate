f01f12d0815ebecaae3d5d39e06f6e2f006fb139
Christopher Jacoby <christopher.a.jacoby@gmail.com>
2023-07-05 15:59:53 -0500

    Corrected the migrations associations to only have belong_to values

--- db/migrate/20230705134758_create_users.rb
+++ db/migrate/20230705134758_create_users.rb
@@ -1,8 +1,6 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-      t.references :profile, foreign_key: true
-      t.references :room, foreign_key: true
       t.string :username, :email
 
       t.timestamps
